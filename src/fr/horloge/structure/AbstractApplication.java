8173dc07559dc83d7278fd13b4946f7cb2a4afdf
Emmanuel NIATI <emmanuel.niati@outlook.fr>
2023-05-18 11:48:42 +0200

    V100 : Initialisation du projet

--- src/fr/horloge/structure/AbstractApplication.java
+++ src/fr/horloge/structure/AbstractApplication.java
@@ -6,7 +6,7 @@ public abstract class AbstractApplication
 	public static final String LOOK_AND_FEEL_METAL = "javax.swing.plaf.metal.MetalLookAndFeel";
 	public static final String LOOK_AND_FEEL_NIMBUS = "javax.swing.plaf.nimbus.NimbusLookAndFeel";
 	public static final String LOOK_AND_FEEL_WINDOWS = "com.sun.java.swing.plaf.windows.WindowsLookAndFeel";
-
+	
 	public AbstractApplication()
 	{
 
